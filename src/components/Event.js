4989a5ed5d521d98bc73a68eeb604d882f438871
DanB815 <>
2021-04-15 01:58:21 +0000

    Fixed "Join" Button - will increase numPeople when clicked (and made data integers)

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,14 +1,23 @@
-
+import { useState, useEffect } from "react";
 
 export default function Event({ title, time, location , numJoined , maxNumber }) {
+
+//let numPeople = numJoined;
+
+  const [numPeople, setNumPeople ] = useState(numJoined);
+
+ useEffect(() =>{
+    console.log(numPeople);
+  }, [numPeople]);
 
 return (
             <div>
             <h4> {title} </h4>
             <h5> {location} || {time} </h5>
-            <h5> Spots: {numJoined} / {maxNumber} </h5>
-            <input type = "button" id = "joinButton" name = "joinButton" value = "Join" onClick = {() => numJoined = numJoined + 1}/>
+            <h5> Spots: {numPeople} / {maxNumber} </h5>
+            <input type = "button" id = "joinButton" name = "joinButton" value = "Join" onClick = {() => setNumPeople(numPeople + 1)}/>
             
             </div>
 );
 }
+
