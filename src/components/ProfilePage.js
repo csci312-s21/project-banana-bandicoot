c4915341d77fa7a57c348f46af769117f12669ea
leilym1 <>
2021-05-04 04:46:24 +0000

    -worked on Profile Page and Login Page

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -8,19 +8,22 @@ export default function ProfilePage({setPage, person}){
 //img
 
 return (
-  <div>
-  <div className={styles.left}>
-        <p onClick={() => { setPage("main"); }}> Hobby Buddy </p>
+  <div className={styles.body}>
+  <div className={styles.b2mp}>
+        <p onClick={() => { setPage("main"); }}> Back </p>
     </div>
     <div className={styles.center}>
-        <h1>{profileData[person].name}</h1>
+        <h1 className={styles.name}>{profileData[person].name}</h1>
+        <div className={styles.info}>
         <p><b>Hometown:</b> {profileData[person].hometown}</p>
         <p><b>Birthday:</b> {profileData[person].birthday}</p>
         <p><b>Major(s):</b> {profileData[person].major}</p>
         <p><b>Class of:</b> {profileData[person].year}</p>
         <p><b>Favorite Hobbies:</b> {profileData[person].hobby}</p>
         <p><b>Bio:</b> {profileData[person].bio}</p>
+        </div>
     </div>  
   </div> 
+
   );
 }
