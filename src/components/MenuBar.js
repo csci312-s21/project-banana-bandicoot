1e57395a796838d64894faa3e35b5e0d50c9b5e4
leilym1 <>
2021-05-11 05:08:38 +0000

    --had some free time, worked on some style

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -18,7 +18,7 @@ export default function MenuBar({allHobbies, children}){
 
   const menu =( 
     <div className={styles.container}>
-    &emsp;&emsp;Hobby Buddy
+    &emsp;&emsp;<div className={styles.heading}></div>
     <br/>
     <button className = {styles.button1}>+Add Event</button>
     <ul className={styles.ul}>
