fd6f990a034e9d92f7227e57824ccfd81fe13b23
Replit user <replituser@example.com>
2021-05-23 16:32:28 +0000

    fixed the profile picture

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -10,16 +10,19 @@ export default function ProfilePage({session, person, setPage}){
 
  
   return (
-      <div>
-       <div className={styles.left} />
+    <div>
+      <div className={styles.left} />
          <div className={styles.center}>
+          <div>
+          <img src={session.user.image} alt= "Image" height="100" width="100"/>
            <h1 className={styles.name}>{person.name}</h1>
+           </div>
            <div className={styles.info}>
            <p><b>Email:</b> {person.email}</p>
            <p><b>Favorite Hobbies:</b> {person.hobby}</p>
-           <div><p><b>Picture:</b></p> {session.user.image}</div>
-           </div>
-         <input className={styles.button} type = "button" id = "AddHobby" value = "AddHobby" onClick = {() => setPage("add")}/>
+           <input className={styles.button} type = "button" id = "AddHobby" value = "AddHobby" onClick = {() => setPage("add")}/>
+          
+         </div>
       </div>
    </div> 
      );
